649c68031c923e69f72bd90314511b06d18d603c
cbronzo <cbronzo@mac.com>
2018-08-18 12:41:37 -0700

    Initial build out of State Controller Routes

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,44 @@
+class StatesController < ApplicationController
+
+#define routes
+
+  #index page
+  get '/states' do
+
+  end
+
+
+  #get one state
+  get '/states/:id' do
+
+  end
+
+  #to edit a state
+  get '/states/:id/edit' do
+
+  end
+
+  #form to create state?
+  get '/states/new' do
+
+  end
+
+  #to update a state
+  patch '/states/:id' do
+
+  end
+
+  #to create a state?
+  post '/states' do
+
+  end
+
+  #delete a state
+  delete '/states/:id' do
+
+  end
+
+
+
+
+end
