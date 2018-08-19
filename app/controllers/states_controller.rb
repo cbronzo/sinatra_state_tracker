2603f0d446f262da7ca7bf8b5106ab31a6961d3d
cbronzo <cbronzo@mac.com>
2018-08-19 10:52:12 -0700

    Re-coding the controllers

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,57 +2,139 @@ require 'pry'
 
 class StatesController < ApplicationController
 
-#define routes
 
-  #index page
   get '/states' do
-
-  end
-
-  #form to create state?
-  get '/states/new' do
-    erb :'states/new'
-  end
-
-
-  #get one state
-  get '/states/:id' do
-
-    erb :'states/show'
-  end
-
-  #to edit a state
-  get '/states/:id/edit' do
-
-  end
-
-
-
-  #to update a state
-  patch '/states/:id' do
-
-  end
-
-  #to create a state?
-  post '/states' do
-    binding.pry
-
-    @state = State.new(name: params[:name], region: params[:region], abbreviation: params[:abbreviation])
-
-    if @state.save
-      redirect '/states/#{@state.id}'
+    if logged_in?
+      @user = current_user
+      @states = State.all
+      erb :'states/states'
     else
-      erb :'/states/new'
+      redirect to '/login'
+    end
+  end
+
+
+  get '/states/new' do
+    if logged_in?
+      erb :'/states/create'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+  post '/states' do
+      if logged_in? && params[:state_name] != ""
+        @State = State.create(:state_name => params[:state_name])
+        @state.user_id = current_user.id
+        @state.save
+      else
+        binding.pry
+        redirect to "/states/new"
+      end
     end
 
+
+  get '/states/:id' do
+    binding.pry
+    if logged_in?
+      @state = State.find_by_id(params[:id])
+      erb :'/states/show'
+    else
+      redirect to '/login'
+    end
   end
 
-  #delete a state
-  delete '/states/:id' do
-
+  get '/states/:id/edit' do
+    if logged_in?
+      @state = State.find_by_id(params[:id])
+      if @state && @state.user == current_user
+        erb :'states/edit'
+      else
+        redirect to '/states'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
 
+    patch '/states/:id' do
+      @state = State.find_by_id(params[:id])
+        if params[:state_name] != ""
+          @state.update(:state_name => params[:state_name])
+          redirect to "/states/#{@state.id}"
+        else
+          redirect to "/states/#{@state.id}/edit"
+        end
+    end
+
+    delete '/states/:id/delete' do
+      if logged_in?
+        @state = State.find_by_id(params[:id])
+          if @state && @state.user == current_user
+              @state.delete
+          end
+        redirect to '/states'
+      else
+        redirect to '/login'
+      end
+  end
 
 
 end
+
+
+# #define routes
+#
+#   #index page
+#   get '/states' do
+#     @states = State.all
+#   end
+#
+#   #form to create state?
+#   get '/states/new' do
+#     @states = State.all
+#     erb :'states/new'
+#   end
+#
+#
+#   #get one state
+#   get '/states/:id' do
+#     @state = State.find_by_id(params[:id])
+#     # binding.pry
+#     erb :'states/show'
+#   end
+#
+#
+#
+#   #to edit a state
+#   get '/states/:id/edit' do
+#
+#   end
+#
+#   #to create a state?
+#   post '/states' do
+#     @state = State.new(params["state"])
+#
+#     if @state.save
+#       redirect '/states/#{@state.id}'
+#     else
+#       erb :'/states/new'
+#     end
+#
+#   end
+#
+#   #to update a state
+#   patch '/states/:id' do
+#
+#   end
+#
+#
+#
+#   #delete a state
+#   delete '/states/:id' do
+#
+#   end
+#
+#
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,103 @@
+require 'pry'
 class UsersController < ApplicationController
 
-
-    #users index route
-    get '/users' do
-      @users = User.all
-      erb :'users/index'
+  get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'/user/show'
     end
 
-    #create new user
-    get '/users/new' do
-      erb :'users/new'
+    get '/signup' do
+      if logged_in?
+        redirect to '/states'
+      end
+      erb :'/users/create_user'
     end
 
-    #submit new user
-    post '/users/new'
-    @user = User.new(params["user"])
-      if @user.save
-        redirect "/users"
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
       else
-        redirect "/users/new"
+        @user = User.create(params)
+        session[:id] = @user.id
+        redirect :'/states'
       end
     end
 
-    #edit user
-    get '/users/:id/edit'
-      @user = User.find_by_id(params[:id])
-      erb :'users/edit'
+    get '/login' do
+      if logged_in?
+        redirect to '/states'
+      end
+        erb :'/users/login'
     end
 
-    #to update a user
-    patch '/users/:id' do
-    end
+      post '/login' do
+        @user = User.find_by(:username => params[:username])
+         if @user && @user.authenticate(params[:password])
+            session[:id] = @user.id
+            redirect :'/states'
+          else
+            redirect to '/signup'
+         end
+      end
+
+      get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to '/login'
+        else
+          redirect to '/'
+        end
+      end
 
 
-    #delete a user
-    delete '/userss/:id' do
+  end
 
-    end
 
-end
+    # #users index route
+    # get '/users' do
+    #   @users = User.all
+    #   erb :'users/index'
+    # end
+    #
+    # #create new user
+    # get '/users/new' do
+    #   erb :'users/new'
+    # end
+    #
+    # #submit new user
+    # post '/users/new' do
+    # @user = User.new(params["user"])
+    #   if @user.save
+    #     redirect "/users"
+    #   else
+    #     redirect "/users/new"
+    #   end
+    # end
+    #
+    # # edit user
+    # get '/users/:id/edit' do
+    #   @user = User.find_by_id(params[:id])
+    #   erb :'users/edit'
+    # end
+    #
+    # #to update a user
+    # patch '/users/:id' do
+    #   @user = User.find_by_id(params[:id])
+    #     if @user.update(params["user"])
+    #       redirect "/users"
+    #     else
+    #       redirect "users/#{user.id}/edit"
+    #     end
+    #
+    # end
+    #
+    # #delete a user
+    # delete '/userss/:id/delete' do
+    #   @user = User.find_by_id(params[:id])
+    #     if @user
+    #       @user.delete
+    #     end
+    #   redirect '/users'
+    #
+    #
+    # end
